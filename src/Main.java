8fe93af8b8492407f1e73bc9bad092164fb7cd84
Elena Ermakova <ermakova.elena058@gmail.com>
2022-07-16 13:13:46 +0300

    commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int cost = 50_000;
+
+        int bonus = 20;
+
+        int gift = cost / bonus;
+
+        System.out.println("Итоговый бонус: " + gift);
+    }
+}
