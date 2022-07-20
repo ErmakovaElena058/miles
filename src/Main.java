afc9f8a7a2e388ab5f4a7cafab1e48f288d8ff9a
Elena Ermakova <ermakova.elena058@gmail.com>
2022-07-20 20:35:17 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,9 @@
 public class Main {
 
-    public static void main(String[] args) {
-        int cost = 50_000;
-
-        int bonus = 20;
-
-        int gift = cost / bonus;
-
-        System.out.println("Итоговый бонус: " + gift);
+        public static void main(String[] args) {
+            BonusMilesService service = new BonusMilesService();
+            int price = 10_000;
+            int miles = service.calculate(price);
+            System.out.println(miles);
+        }
     }
-}
